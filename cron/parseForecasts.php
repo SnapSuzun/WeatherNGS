c3ba8258f2ec179d44622d4c6e8579e909206023
Nevzorov georgiy <nevzorovgosha@gmail.com>
2016-08-29 15:06:42 +0700

    fix

--- cron/parseForecasts.php
+++ cron/parseForecasts.php
@@ -13,25 +13,29 @@ echo "\r\rSTART...\r\n";
 
 $cities = City::collection()->find();
 
-foreach ($cities as $city) {
+try {
+    foreach ($cities as $city) {
 
-    $forecast = WeatherAPI::getCurrentForecast($city['alias']);
+        $forecast = WeatherAPI::getCurrentForecast($city['alias']);
 
-    if (isset($forecast['errors'])) {
-        throw new \Exception($forecast['errors']['message']);
+        if (isset($forecast['errors'])) {
+            throw new \Exception($forecast['errors']['message']);
+        }
+
+        $forecast = array_shift($forecast['forecasts']);
+
+        $cursor = Forecast::collection()->find([
+            'links' => ['city' => $city['alias']],
+            'update_date' => $forecast['update_date']
+        ]);
+
+        if ($cursor->count() == 0) {
+            $forecast['date'] = new \MongoDate(strtotime($forecast['date']));
+            Forecast::collection()->save($forecast);
+        }
     }
-
-    $forecast = array_shift($forecast['forecasts']);
-
-    $cursor = Forecast::collection()->find([
-        'links' => ['city' => $city['alias']],
-        'update_date' => $forecast['update_date']
-    ]);
-
-    if ($cursor->count() == 0) {
-        $forecast['date'] = new \MongoDate(strtotime($forecast['date']));
-        Forecast::collection()->save($forecast);
-    }
+} catch(Exception $e) {
+    echo $e->getMessage() . "\r\n";
 }
 
 echo "\r\rEND\r\n";
